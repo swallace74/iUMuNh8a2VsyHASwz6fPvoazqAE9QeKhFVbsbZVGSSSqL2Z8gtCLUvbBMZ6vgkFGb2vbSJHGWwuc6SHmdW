854c05223b779bb81255c4eafb5fd9b1cde6312f
JimBobUKII <jimbobuk@me.com>
2021-04-19 17:53:51 +0100

    Update eVersionLookup.ps1

--- eVersionLookup.ps1
+++ eVersionLookup.ps1
@@ -1,6 +1,6 @@
 $Script:eVersions = @(
     [pscustomobject]@{ VersionName = '2019'; LatestBuild = '15.02.0860'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2019'; }
     [pscustomobject]@{ VersionName = '2016'; LatestBuild = '15.01.2248'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2016'; }
-    [pscustomobject]@{ VersionName = '2013'; LatestBuild = '15.00.1497'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2013'; }
+    [pscustomobject]@{ VersionName = '2013'; LatestBuild = '15.00.1500'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2013'; }
     [pscustomobject]@{ VersionName = '2010'; LatestBuild = '14.03.0601'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2010'; }
 )
