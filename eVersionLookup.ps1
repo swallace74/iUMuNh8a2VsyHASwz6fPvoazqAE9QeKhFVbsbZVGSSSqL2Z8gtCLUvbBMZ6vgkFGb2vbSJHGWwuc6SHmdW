0be4e5f01e34c74126337ae875c42d84871a0587
JimBobUKII <jimbobuk@me.com>
2021-05-25 12:00:51 +0100

    Update eVersionLookup.ps1

--- eVersionLookup.ps1
+++ eVersionLookup.ps1
@@ -3,6 +3,6 @@
 $Script:eVersions = @(
     [pscustomobject]@{ VersionName = '2019'; LatestBuild = '15.02.0858a'; Name = 'Cumulative Update 9 May 21'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2019'; }
     [pscustomobject]@{ VersionName = '2016'; LatestBuild = '15.01.2242a'; Name = 'Cumulative Update 20 May 21'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2016'; }
-    [pscustomobject]@{ VersionName = '2013'; LatestBuild = '15.00.1497a'; Name = 'Cumulative Update 23 May 21'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2013'; }
+    [pscustomobject]@{ VersionName = '2013'; LatestBuild = '15.00.1497.018'; Name = 'Cumulative Update 23 May 21'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2013'; }
     [pscustomobject]@{ VersionName = '2010'; LatestBuild = '14.03.0513'; Name = 'Update Rollup 32'; Url = 'https://docs.microsoft.com/en-us/exchange/new-features/build-numbers-and-release-dates?view=exchserver-2019#exchange-server-2010'; }
 )
